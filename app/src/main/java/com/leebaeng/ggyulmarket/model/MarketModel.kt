a1566dfb2a0751c7faafc6c93e57ec902a78461d
aqwer4 <lbh8579@gmail.com>
2022-03-13 01:03:59 +0900

    feat : UserModel 추가 및 MarketModel 업데이트  - MarketModel 업데이트    . isPriceProposeAble, viewCnt, likedUserList, talkedUserList 필드 추가    . bundle param으로 넘기기 위한 Serializable 상속

--- app/src/main/java/com/leebaeng/ggyulmarket/model/MarketModel.kt
+++ app/src/main/java/com/leebaeng/ggyulmarket/model/MarketModel.kt
@@ -1,13 +1,19 @@
 package com.leebaeng.ggyulmarket.model
 
+import java.io.Serializable
+
 data class MarketModel(
     val sellerId: String = "",
     val title: String = "",
     val createdAt: Long = 0L,
     val price: Long = 0L,
     val description: String = "",
+    val isPriceProposeAble: Boolean? = false,
     val imgUrl: List<String>? = null,
     val talkCnt: Int? = null,
     val likeCnt: Int? = null,
+    val viewCnt: Int? = null,
+    val likedUserList: List<String>? = null,
+    val talkedUserList: List<String>? = null,
     val id: String = "$sellerId-$createdAt"
-)
+): Serializable
---
+++ app/src/main/java/com/leebaeng/ggyulmarket/model/UserModel.kt
@@ -0,0 +1,8 @@
+package com.leebaeng.ggyulmarket.model
+
+data class UserModel (
+    val id: String,
+    val nickName: String,
+    val locationName: String,
+    val manner : Float
+)
