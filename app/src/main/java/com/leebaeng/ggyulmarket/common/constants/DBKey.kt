f189c785caf1f60f829102165cd91a4abc4dba0e
aqwer4 <lbh8579@gmail.com>
2022-03-13 01:02:40 +0900

    feat : 테스트용 DB-Key 추가

--- app/src/main/java/com/leebaeng/ggyulmarket/common/constants/DBKey.kt
+++ app/src/main/java/com/leebaeng/ggyulmarket/common/constants/DBKey.kt
@@ -1,5 +1,6 @@
 package com.leebaeng.ggyulmarket.common.constants
 
 object DBKey {
-    const val TABLE_MARKET_LIST = "Market"
+//    const val TABLE_MARKET_LIST = "Market"
+    const val TABLE_MARKET_LIST = "Market_Test"
 }
