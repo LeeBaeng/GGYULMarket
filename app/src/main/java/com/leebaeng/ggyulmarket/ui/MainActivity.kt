534f36a45a9c67cc87c93b41b30063120943bec0
aqwer4 <lbh8579@gmail.com>
2022-03-14 02:51:47 +0900

    refactor : MainActivity 업데이트(fragment 관리로직 추가, Hilt 적용)

--- app/src/main/java/com/leebaeng/ggyulmarket/ui/MainActivity.kt
+++ app/src/main/java/com/leebaeng/ggyulmarket/ui/MainActivity.kt
@@ -8,40 +8,50 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.leebaeng.ggyulmarket.R
+import com.leebaeng.ggyulmarket.common.Constants
+import com.leebaeng.ggyulmarket.di.DBAdapter
 import com.leebaeng.ggyulmarket.ui.chatlist.ChatListFragment
 import com.leebaeng.ggyulmarket.ui.home.HomeFragment
+import com.leebaeng.ggyulmarket.ui.login.JoinFragment
 import com.leebaeng.ggyulmarket.ui.login.LoginFragment
 import com.leebaeng.ggyulmarket.ui.mypage.MyPageFragment
 import com.leebaeng.util.log.LLog
+import com.leebaeng.util.log.logE
 import com.leebaeng.util.log.logS
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity() {
+    @Inject lateinit var dbAdapter: DBAdapter
+    private val fragmentMap = hashMapOf<String, Fragment>(
+        Constants.FRAGMENT_ID_HOME to HomeFragment(),
+        Constants.FRAGMENT_ID_CHAT_LIST to ChatListFragment(),
+        Constants.FRAGMENT_ID_LOGIN to LoginFragment(),
+        Constants.FRAGMENT_ID_JOIN to JoinFragment(),
+        Constants.FRAGMENT_ID_MY_PAGE to MyPageFragment()
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LLog.init(this)
 
         setContentView(R.layout.activity_main)
-
-        val homeFragment = HomeFragment()
-        val chatListFragment = ChatListFragment()
-        val myPageFragment = MyPageFragment()
-        val loginFragment = LoginFragment()
-
-        replaceFragment(homeFragment)
+        replaceFragment(fragmentMap[Constants.FRAGMENT_ID_HOME])
 
         val bottomNavView = findViewById<BottomNavigationView>(R.id.navViewBottom)
         bottomNavView.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.home -> {
-                    replaceFragment(homeFragment)
+                    replaceFragment(fragmentMap[Constants.FRAGMENT_ID_HOME])
                     return@setOnItemSelectedListener true
                 }
                 R.id.chatList -> {
-                    replaceFragment(chatListFragment)
+                    replaceFragment(fragmentMap[Constants.FRAGMENT_ID_CHAT_LIST])
                     return@setOnItemSelectedListener true
                 }
                 R.id.myPage -> {
-                    replaceFragment(myPageFragment)
+                    replaceFragment(fragmentMap[Constants.FRAGMENT_ID_MY_PAGE])
                     return@setOnItemSelectedListener true
                 }
             }
@@ -56,7 +66,13 @@ class MainActivity : BaseActivity() {
         Firebase.database.reference.child("Users").child("ID")
     }
 
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
+    fun replaceFragment(id: String) {
+        replaceFragment(fragmentMap[id])
+    }
+
+    fun replaceFragment(fragment: Fragment?) {
+        if(fragment != null){
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
+        }else "ReplaceFragment Failed!! Fragment is null!".logE()
     }
 }
