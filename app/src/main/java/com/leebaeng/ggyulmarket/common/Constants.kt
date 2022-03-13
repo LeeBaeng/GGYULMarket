9e45b45f1a61ca9faff75cb0e52d52607b0d1faf
aqwer4 <lbh8579@gmail.com>
2022-03-14 02:42:59 +0900

    refactor : Constants 통합 및 네이밍 변경 / Constants 패키지 경로 변경

---
+++ app/src/main/java/com/leebaeng/ggyulmarket/common/Constants.kt
@@ -0,0 +1,19 @@
+package com.leebaeng.ggyulmarket.common
+
+object Constants {
+    // FireStore Database Table constants
+//    const val TABLE_MARKET = "Market"
+    const val FS_TABLE_MARKET = "Market_Test"
+    const val FS_TABLE_USER = "User"
+
+    // Realtime Database Table constants
+    const val RT_TABLE_CHAT = "Chat"
+
+    // Fragment Constants
+    const val FRAGMENT_ID_HOME = "HomeFragment"
+    const val FRAGMENT_ID_CHAT = "ChatFragment"
+    const val FRAGMENT_ID_CHAT_LIST = "ChatRoomListFragment"
+    const val FRAGMENT_ID_MY_PAGE = "MyPageFragment"
+    const val FRAGMENT_ID_LOGIN = "LoginFragment"
+    const val FRAGMENT_ID_JOIN = "JoinFragment"
+}
--- app/src/main/java/com/leebaeng/ggyulmarket/common/constants/DBKey.kt
+++
@@ -1,6 +0,0 @@
-package com.leebaeng.ggyulmarket.common.constants
-
-object DBKey {
-//    const val TABLE_MARKET_LIST = "Market"
-    const val TABLE_MARKET_LIST = "Market_Test"
-}
