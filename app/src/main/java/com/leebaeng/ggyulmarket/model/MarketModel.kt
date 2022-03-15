a20c335f9fbef14471a896dbdedbcd0709a5aaa0
aqwer4 <lbh8579@gmail.com>
2022-03-16 02:17:03 +0900

    feat : MarketModel(likeCnt 제거 및 Serializable 제거), UserModel(Firebase를 위한 빈 생성자 추가) 업데이트

--- app/src/main/java/com/leebaeng/ggyulmarket/model/MarketModel.kt
+++ app/src/main/java/com/leebaeng/ggyulmarket/model/MarketModel.kt
@@ -1,7 +1,5 @@
 package com.leebaeng.ggyulmarket.model
 
-import java.io.Serializable
-
 data class MarketModel(
     val sellerId: String = "",
     val title: String = "",
@@ -11,9 +9,8 @@ data class MarketModel(
     val priceProposeAble: Boolean? = false,
     val imgUrl: List<String>? = null,
     val talkCnt: Int? = null,
-    val likeCnt: Int? = null,
     val viewCnt: Int? = null,
     val likedUserList: List<String>? = null,
     val talkedUserList: List<String>? = null,
     val id: String = "$sellerId-$createdAt"
-): Serializable
+)
--- app/src/main/java/com/leebaeng/ggyulmarket/model/UserModel.kt
+++ app/src/main/java/com/leebaeng/ggyulmarket/model/UserModel.kt
@@ -1,9 +1,9 @@
 package com.leebaeng.ggyulmarket.model
 
 data class UserModel(
-    val id: String,
-    val nickName: String,
-    val location: String,
+    val id: String = "",
+    val nickName: String = "",
+    val location: String = "",
     val manner: Float = 36.5f,
     val profileImgUrl: String? = null,
     val sellItemIdList: HashMap<String, String>? = null, // SellItem ID Map. value is possible get data from DB and convert to <MarketModel> Object
